71a4ff6b1e8b4d8e7720072037d555f3c6a32080
JSpru <jgspruance@gmail.com>
2023-05-08 17:23:43 -0700

    Update app.py
    Update server

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello world! This Python Flask app is brought to you by Dome Global"
+
+if __name__ == "__main__":
+    app.run()
+
+def run():
+    port = int(os.getenv("PORT"))
+    app.run(host='0.0.0.0', port=port)
